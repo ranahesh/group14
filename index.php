dea00c18a63555689c8c4c3a6878c92f0e15a4de
ranahesh <105362160+ranahesh@users.noreply.github.com>
2022-06-05 20:43:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Swap two numbers without using a temporary variable.
+ex: $num1 = 10;  $num2 = 20; 
+output : $num1 = 20;  $num2 = 10; 
+*/
+
+$x=20;
+$y=10;
+$x-$y+$x;
+echo "value of x:$x";
+ echo "<br>";
+ $x-$x-$x;
+echo "<br>";
+ echo "value of x:$y";
+ echo "<br>";
+
+
+
+ 
+
+// task 2   
+//Write a program to print 2 php variables using single echo statement. 
+$message1 = "Good Morning.";
+$message2 = "anice day!";
+echo $message1.$message2
+?>
+
